6ef88dd70929150d2a38b92f31f4c99bffd766f7
Robert Weber <wwwdaze2000@gmail.com>
2019-07-14 10:22:04 -0400

    Update psSeqRep.ps1

--- psSeqRep.ps1
+++ psSeqRep.ps1
@@ -9,8 +9,10 @@ Begin{
     class seqRep{
         $op = @( '+', '-', '/', '*', ' ' )                
         $numbers = @(0,1,2,3,4,5,6,7,8,9)
+        $results = @{}
         $begin = 0
         $end = 0
+        $starttime = (get-date)
         $logfile = "seqRep.log"
         
         seqRep($begin, $end, $logFile){
@@ -26,15 +28,32 @@ Begin{
             if($this.logFile -ne $null -and $this.logfile -ne ''){
                 $eqn | add-content "$($this.logfile)"
             }
-            try{
-                $sol = iex $eqn -errorAction silentlyContinue
-                if($sol -eq $search){
-                    return $eqn
+            
+                if($this.results.keys -contains ([int]$search)){
+                    return $this.results[ ([int]$search) ]
                 }else{
-					return ''
-				}
-            }catch{}
-            return ''
+                    try{
+                        $sol = iex $eqn -errorAction silentlyContinue
+                    }catch{
+                        $sol = -1
+                    }
+                    
+                    
+                    if($sol -eq [math]::floor($sol) -and $sol -ge $this.begin -and $sol -le $this.end  ){
+                        if(!($this.results.keys -contains ([int]$sol) )){
+                            $this.results.add(
+                                ([int]$sol), 
+                                $eqn
+                            )
+                        }
+                    }
+                    if($sol -eq $search){
+                        return $eqn
+                    }else{
+                        return ''
+                    }
+                }
+            
         }
         
         [string] getEqn($a, $b, $c, $d, $e, $f, $g, $h, $i){
@@ -85,6 +104,7 @@ Begin{
         
         execute(){
             $this.loop()
+            write-host ([timespan]( (get-date) - $this.starttime))
         }
         
         loop(){
@@ -92,124 +112,70 @@ Begin{
             for($search = $this.begin; $search -le $this.end; $search++){
                 $found = $false
                 $iterations = 0
-                :search for($a = 0; $a -lt 5; $a++){
-                    for($b = 0; $b -lt 5; $b++){
-                        for($c = 0; $c -lt 5; $c++){
-                            for($d = 0; $d -lt 5; $d++){
-                                for($e = 0; $e -lt 5; $e++){
-                                    for($f = 0; $f -lt 5; $f++){
-                                        for($g = 0; $g -lt 5; $g++){
-                                            for($h = 0; $h -lt 5; $h++){
-												for($i = 0; $i -lt 5; $i++){
-													write-host '.' -noNewLine
-													$iterations++
-													
-													$opsIndex  = [System.Collections.Generic.List[System.Object]]( @($a, $b, $c, $d, $e, $f, $g, $h, $i) )
-													for($rot = 0; $rot -lt 9; $rot++){
-														$o1 = $opsIndex[0];
-														$opsIndex.removeAt(0)
-														$opsIndex.add($o1)
+                while($iterations -lt 1000 -and $found -eq $false){
+                    clear
+                    write-host "Searching for: $($search)"
+                    $this.results.getEnumerator() | sort Name | ft | out-string | write-host
+                    
+                    $iterations++
+                    $a = (get-random -minimum 0 -maximum 5)
+                    $b = (get-random -minimum 0 -maximum 5)
+                    $c = (get-random -minimum 0 -maximum 5)
+                    $d = (get-random -minimum 0 -maximum 5)
+                    $e = (get-random -minimum 0 -maximum 5)
+                    $f = (get-random -minimum 0 -maximum 5)
+                    $g = (get-random -minimum 0 -maximum 5)
+                    $h = (get-random -minimum 0 -maximum 5)
+                    $i = (get-random -minimum 0 -maximum 5)
+                    $opsIndex  = [System.Collections.Generic.List[System.Object]]( @($a, $b, $c, $d, $e, $f, $g, $h, $i) )
+                    for($rot = 0; $rot -lt 9; $rot++){
+                        $o1 = $opsIndex[0];
+                        $opsIndex.removeAt(0)
+                        $opsIndex.add($o1)
+                        $a, $b, $c, $d, $e, $f, $g, $h, $i = $opsIndex
+                        
+                        if($found -eq $false){
+                            $eqn = $this.getEqn($a, $b, $c, $d, $e, $f, $g, $h, $i)
+                            if($this.testEqn($eqn, $search) -ne ''){
+                                $found = $true
+                            }
+                        }
+                    }
+                                                        
+                    #if multiplication or division is present, try with parenthesis
+                    if(
+                        $this.op[$a] -eq '/' -or $this.op[$a] -eq '*' -or
+                        $this.op[$b] -eq '/' -or $this.op[$b] -eq '*' -or
+                        $this.op[$c] -eq '/' -or $this.op[$c] -eq '*' -or
+                        $this.op[$d] -eq '/' -or $this.op[$d] -eq '*' -or
+                        $this.op[$e] -eq '/' -or $this.op[$e] -eq '*' -or
+                        $this.op[$f] -eq '/' -or $this.op[$f] -eq '*' -or
+                        $this.op[$g] -eq '/' -or $this.op[$g] -eq '*' -or
+                        $this.op[$h] -eq '/' -or $this.op[$h] -eq '*' -or
+                        $this.op[$i] -eq '/' -or $this.op[$i] -eq '*'
+                    ){                                              
+                        # add paranthesis grouping to selections of numbers
+                        for($first = 0; $first -lt 8; $first++){
+                            for($last = 9; $last -gt $first; $last--){
+                                
+                                $opsIndex  = [System.Collections.Generic.List[System.Object]]( @($a, $b, $c, $d, $e, $f, $g, $h, $i) )
+                                for($rot = 0; $rot -lt 9; $rot++){
+                                    $o1 = $opsIndex[0];
+                                    $opsIndex.removeAt(0)
+                                    $opsIndex.add($o1)
 
-														$a, $b, $c, $d, $e, $f, $g, $h, $i = $opsIndex
-														
-														$eqn = $this.getEqn($a, $b, $c, $d, $e, $f, $g, $h, $i)
-														# write-host $eqn
-														if($this.testEqn($eqn, $search) -ne ''){
-															$found = $true
-															write-host ""
-															write-host "$($eqn) --> $($search)"
-															write-host "Found at index: $($iterations)"
-															break search
-														}
-													}
-													
-													$opsIndex  = [System.Collections.Generic.List[System.Object]]( @($a, $b, $c, $d, $e, $f, $g, $h, $i) )
-													for($rot = 0; $rot -lt 9; $rot++){
-														$o1 = $opsIndex[0];
-														$opsIndex.removeAt(0)
-														$opsIndex.add($o1)
-
-														$a, $b, $c, $d, $e, $f, $g, $h, $i = $opsIndex
-														
-														$eqn = $this.getEqn($i, $h, $g, $f, $e, $d, $c, $b, $a)
-														# write-host $eqn
-														if($this.testEqn($eqn, $search) -ne ''){
-															$found = $true
-															write-host ""
-															write-host "$($eqn) --> $($search)"
-															write-host "Found at index: $($iterations)"
-															break search
-														}
-													
-													}
-													
-													#if multiplication or division is present, try with parenthesis
-													if(
-														$this.op[$a] -eq '/' -or $this.op[$a] -eq '*' -or
-														$this.op[$b] -eq '/' -or $this.op[$b] -eq '*' -or
-														$this.op[$c] -eq '/' -or $this.op[$c] -eq '*' -or
-														$this.op[$d] -eq '/' -or $this.op[$d] -eq '*' -or
-														$this.op[$e] -eq '/' -or $this.op[$e] -eq '*' -or
-														$this.op[$f] -eq '/' -or $this.op[$f] -eq '*' -or
-														$this.op[$g] -eq '/' -or $this.op[$g] -eq '*' -or
-														$this.op[$h] -eq '/' -or $this.op[$h] -eq '*' -or
-														$this.op[$i] -eq '/' -or $this.op[$i] -eq '*'
-													){                                              
-														# add paranthesis grouping to selections of numbers
-														for($first = 0; $first -lt 8; $first++){
-															for($last = 9; $last -gt $first; $last--){
-																
-																$opsIndex  = [System.Collections.Generic.List[System.Object]]( @($a, $b, $c, $d, $e, $f, $g, $h, $i) )
-																for($rot = 0; $rot -lt 9; $rot++){
-																	$o1 = $opsIndex[0];
-																	$opsIndex.removeAt(0)
-																	$opsIndex.add($o1)
-
-																	$a, $b, $c, $d, $e, $f, $g, $h, $i = $opsIndex
-																	
-																	$eqn = $this.getEqn($a, $b, $c, $d, $e, $f, $g, $h, $i, $first, $last)
-																	# write-host $eqn
-																	if($this.testEqn($eqn, $search) -ne ''){
-																		$found = $true
-																		write-host ""
-																		write-host "$($eqn) --> $($search)"
-																		write-host "Found at index: $($iterations)"
-																		break search
-																	}
-																}
-																for($rot = 0; $rot -lt 9; $rot++){
-																	$o1 = $opsIndex[0];
-																	$opsIndex.removeAt(0)
-																	$opsIndex.add($o1)
-
-																	$a, $b, $c, $d, $e, $f, $g, $h, $i = $opsIndex
-																	
-																	$eqn = $this.getEqn($i, $h, $g, $f, $e, $d, $c, $b, $a, $first, $last)
-																	# write-host $eqn
-																	if($this.testEqn($eqn, $search) -ne ''){
-																		$found = $true
-																		write-host ""
-																		write-host "$($eqn) --> $($search)"
-																		write-host "Found at index: $($iterations)"
-																		break search
-																	}
-																}
-																
-															}
-														}
-													}
-													
-													if($iterations % 25 -eq 0){
-														write-host $eqn
-													}
-												}
-                                            }
+                                    $a, $b, $c, $d, $e, $f, $g, $h, $i = $opsIndex
+                                    if($found -eq $false){
+                                        $eqn = $this.getEqn($a, $b, $c, $d, $e, $f, $g, $h, $i, $first, $last)
+                                        if($this.testEqn($eqn, $search) -ne ''){
+                                            $found = $true
                                         }
                                     }
                                 }
                             }
                         }
                     }
+                    
                 }
                 if(!$found){
                     write-host "$($search) could not be found"
@@ -221,6 +187,7 @@ Begin{
 Process{
     clear;
     $error.clear()
+    
     $seqRep = [seqRep]::new($begin, $end, $logfile);
     $seqRep.execute()
 }
